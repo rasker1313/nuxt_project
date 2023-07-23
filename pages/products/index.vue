b65062e41a7112f052b04aa8562c87d3e2ce08e7
arty1312@gmail.com <Travoman./#1313>
2023-07-23 14:01:47 +0300

    add products from fakestoreapi

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h2>Product</h2>
-    <p>Test text </p>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-definePageMeta({
-  layout: 'products'
-})
+<script setup>
+  definePageMeta({
+    layout: 'products'
+  })
+
+ const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
 </script>
 
 <style scoped>
