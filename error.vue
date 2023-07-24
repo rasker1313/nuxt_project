499fdae0063e6c15d2308b71b21fd435e3b3e982
arty1312@gmail.com <Travoman./#1313>
2023-07-24 10:30:18 +0300

    created page for cache errors

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-7 max-w-md mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+    <p class="mt-7 text-6xl">Ooops</p>
+    <p class="mt-7">{{ error.message }}</p>
+    <button class="btn mt-7" @click="handleClearError">Go Home</button>
+  </div>
+</template>
+
+<script setup>
+  defineProps(['error'])
+
+  const handleClearError = () => clearError({ redirect: '/'})
+</script>
+
+<style scoped>
+
+</style>
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,10 @@ const uri = 'https://fakestoreapi.com/products/' + id
 
 const { data: product } = await useFetch(uri, { key: id })
 
+if (!product.value) {
+  throw createError ({ statusCode: 404, statusMessage: 'Product not found'})  // ,fatal: true - 404 error for broken links
+}
+
 definePageMeta({
   layout: 'products'
 })
