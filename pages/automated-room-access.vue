4a6dea4701f06cc009d4999aa29091fbfc2fbda1
Artur Yurov <arty1312@gmail.com>
2023-08-30 11:02:17 +0300

    additional pages content section

--- pages/automated-room-access.vue
+++ pages/automated-room-access.vue
@@ -17,14 +17,46 @@
         </template>
       </PageHeroSection>
     </div>
+
+    <ContentSection
+        :aboveTitle="'Deploy faster'"
+        :title="'A better workflow'"
+        :description="'Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.'"
+        :outerBlockHeight="700"
+    >
+      <template v-slot:media>
+        <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="" />
+      </template>
+      <template v-slot:content>
+        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+        <ul role="list" class="mt-8 space-y-8 text-gray-600">
+          <li class="flex gap-x-3">
+            <CloudArrowUpIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+            <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+          </li>
+          <li class="flex gap-x-3">
+            <LockClosedIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+            <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+          </li>
+          <li class="flex gap-x-3">
+            <ServerIcon class="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
+            <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+          </li>
+        </ul>
+        <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
+        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+        <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
+      </template>
+    </ContentSection>
   </div>
 </template>
 
 <script>
 import PageHeroSection from '@/components/Features/PageHeroSection.vue'
+import ContentSection from '@/components/Features/ContentSection.vue'
 
 export default {
-  components: { PageHeroSection },
+  components: { PageHeroSection, ContentSection },
   data(){
     return{
       underDescription: [
@@ -61,5 +93,19 @@ export default {
 </script>
 
 <style scoped>
+.scrollbar::-webkit-scrollbar {
+  width: 10px;
+  background-color: #f9f9fd;
+}
 
+.scrollbar::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  background-color: rgb(37, 99, 235);
+}
+
+.scrollbar::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.2);
+  border-radius: 10px;
+  background-color: #f9f9fd;
+}
 </style>
